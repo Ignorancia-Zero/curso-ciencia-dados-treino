16dd1ff9f632c420dbf32bde7bb4b41aac5f2b6a
Ignorância Zero <ignorancia.zero.2021@gmail.com>
2022-05-02 14:29:34 -0300

    adiciona a base de docentes

--- src/aquisicao/opcoes.py
+++ src/aquisicao/opcoes.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from src.aquisicao.inep.docente import DocenteETL
 from src.aquisicao.inep.escola import EscolaETL
 from src.aquisicao.inep.gestor import GestorETL
 from src.aquisicao.inep.turma import TurmaETL
@@ -9,10 +10,11 @@ class EnumETL(Enum):
     escola = "ESCOLA"
     gestor = "GESTOR"
     turma = "TURMA"
+    docente = "DOCENTE"
 
 
 # lista os objetos ETL que fazem parte dos micro-dados do inep
-ETL_INEP_MICRO = [EnumETL.escola, EnumETL.gestor, EnumETL.turma]
+ETL_INEP_MICRO = [EnumETL.escola, EnumETL.gestor, EnumETL.turma, EnumETL.docente]
 
 
 # chave = Enum
@@ -21,4 +23,5 @@ ETL_DICT = {
     EnumETL.escola: EscolaETL,
     EnumETL.gestor: GestorETL,
     EnumETL.turma: TurmaETL,
+    EnumETL.docente: DocenteETL
 }
